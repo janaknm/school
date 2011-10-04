90dc3d88dfc0733eb8c2583d2380d79519ca051a
matt <m@ttforbes.com>
2011-10-04 16:43:17 -0700

    ugh

--- 352/as2/builtin.c
+++ 352/as2/builtin.c
@@ -17,8 +17,8 @@ void bin_exit(int argc, char **argv) {
 void bin_aecho(int argc, char **argv) {
     int print_nl = 1,
         len = 0,
-        tmp_len,
         i = 0;
+    
     char *buf;
         
 
@@ -28,40 +28,21 @@ void bin_aecho(int argc, char **argv) {
         argv++;
         argc--;
     }
-    
-    /* calculate max buffer size */
-    for(i = 1; i < argc; i++) {
-        /* add 2 to account for '\0' and ' ' */
-        tmp_len = strlen(argv[i]) + 2;
-        len = (len > tmp_len) ? len : tmp_len;
-    }
-    
-    /* allocate buffer */
-    buf = (char *)malloc(len * sizeof(char));
-    if (!buf)
-        perror("malloc");
 
-    /* write args to stdout (fd=1) */
     for(i = 1; i < argc; i++) {
-        tmp_len = strlen(argv[i]) + 1;
-
-        /* write arg to string */
-        printf("%s", argv[i]);
-        if (i != 1) printf(" ");
+        if (i != 1) {
+            if (write(1, " ", 1) < 0) 
+                perror("write");
+        }
         
-        /* write to fd */
-        /*
-        if (write(1, buf, tmp_len + ((i==1) ? 0 : 1)) < 0) 
+        if (write(1, argv[i], strlen(argv[i])) < 0) 
             perror("write");
-        */
+        
     }
-
+    
     if (print_nl) {
-        printf("\n");
-        /*
         if (write(1, "\n", 1) < 0) 
             perror("write");
-        */
     }
         
 }
